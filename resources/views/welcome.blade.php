08e9f2e6c7bf54fb12dad5287cb5f14d3a3c8fbe
git push <raulmahya11@gmail.com>
2026-03-31 11:09:50 +0700

    finsi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -494,7 +494,7 @@
           <!-- FOTO / LOGO -->
           <img src="{{ asset('assets/AndalanLogo.png') }}"
   alt="Logo Human Careers"
-  class="h-16 md:h-20 w-auto object-contain">
+  class="h-20 md:h-20 w-auto object-contain">
         </a>
       </div>
 
